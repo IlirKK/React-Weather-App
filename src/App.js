885bb395dede14e9abf8ec592b1fe653c502a65b
IlirKK <kymori@outlook.de>
2024-02-18 00:50:46 +0100

    Weather app finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// const Fetch = () => {const [state, setState] = useState([]);
+//   useEffect(() => {
+//     fetch('http://api.weatherapi.com/v1/current.json?key=553c60f7a8fc4b05819183408241302&q=10001' + '')
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         setState(data);
+//       });
+//   }, []);
+// };
+// export default Fetch;
+
+
+export default function Weather() {
+  const [city, setCity] = useState();
+  const [cityButton, setCityButton] = useState(0);
+  const [response, setResponse] = useState();
+  useEffect(() => {
+    fetch('http://api.weatherapi.com/v1/current.json?key=553c60f7a8fc4b05819183408241302&q=' + city)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json)
+        setResponse(json);
+      })
+    setCity('')
+  }, [cityButton])
+
+  return <div className='weather'>
+
+    <h4>Ilir's Weather API App</h4>
+
+    <div>
+      <input value={city} onChange={(evnt) => { setCity(evnt.target.value) }}></input>
+      <button onClick={(evnt) => { setCityButton(cityButton + 1) }}>Go!</button>
     </div>
-  );
+    <WeatherResponse data={response} ></WeatherResponse>
+
+  </div>
 }
 
-export default App;
+
+function WeatherResponse({ data }) {
+  return <div>
+    <br></br>
+    <div>City: {data?.location?.name}</div>
+    <div>Country: {data?.location?.country}</div>
+    <div>Local Time: {data?.location?.localtime}</div>
+    <div>Weather: {data?.current?.feelslike_c}°C</div>
+  </div>
+}
+
